19ccea46fc62decda85fbe5f76ac33c5a073c561
screw-pack <gaillard.germain@gmail.com>
2020-07-31 13:46:46 +0200

    some adjust

--- js/script.js
+++ js/script.js
@@ -2,31 +2,31 @@ function isOverflown(element) {
   return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
 }
 
-var myNav = document.getElementById("sum_menu");
-var summary = document.getElementById("summary");
+var myNav = document.getElementById('sum_menu');
+var summary = document.getElementById('summary');
 summary.addEventListener('click', function(event) {
-  if (myNav.className === "hidden") {
-    myNav.className = "";
-    myNav.style.left = "0";
-    summary.innerHTML = "Summary <"
+  if (myNav.className === 'hidden') {
+    myNav.className = '';
+    myNav.style.left = '0';
+    summary.innerHTML = 'Summary <'
   } else {
-    myNav.className = "hidden";
-    myNav.style.left = "-35em";
-    summary.innerHTML = "Summary >"
+    myNav.className = 'hidden';
+    myNav.style.left = '-35em';
+    summary.innerHTML = 'Summary >'
   }
 })
 
-var more = document.getElementsByClassName("more");
+var more = document.getElementsByClassName('more');
 for (element of more) {
   element.addEventListener('click', function(event) {
     var myarticle = this.parentElement;
     var content = myarticle.querySelector('.content')
     if (isOverflown(content)) {
-      content.style.maxHeight = "100%";
-      this.innerHTML = "View less";
+      content.style.maxHeight = '100%';
+      this.innerHTML = 'View less';
     } else {
-      content.style.maxHeight = "3em";
-      this.innerHTML = "View more";
+      content.style.maxHeight = '3em';
+      this.innerHTML = 'View more';
     }
   })
 }
